30c86b12753004d0657136007be480d9eb18fe44
sam.sjk <sam.sjk@d8bbe975-9c3a-0410-8092-a9b24094b31a>
2009-02-18 16:06:09 +0000

    reseting this project (all code from extapi is coming over here)

--- trunk/move-me-to-webroot/app/View/templates/courses/baseball_gm/faq.php
+++
@@ -1,5 +0,0 @@
-<?php $payload->title = 'FAQ'; ?>
-<h1><?php echo ($payload->message); ?></h1>
-<pre>
-<?php echo ($payload->controller); ?>
-</pre>
--- trunk/move-me-to-webroot/app/View/templates/courses/baseball_gm/testimonial.php
+++
@@ -1,5 +0,0 @@
-<?php $payload->title = 'boo'; ?>
-<h1><?php echo ($payload->message); ?></h1>
-<pre>
-<?php echo ($payload->controller); ?>
-</pre>
