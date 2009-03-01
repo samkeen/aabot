c227d779fbfc75d8cbcf0c63e910ccd9e702d42e
sam.sjk <sam.sjk@d8bbe975-9c3a-0410-8092-a9b24094b31a>
2009-03-01 00:17:01 +0000

    html() added to Feedback helper.  used in layout to display errors

---
+++ aabot/View/Helper/Html.php
@@ -0,0 +1,15 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Html
+ *
+ * @author sam
+ */
+class View_Helper_Html {
+    
+}
+?>
