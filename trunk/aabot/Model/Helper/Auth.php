78b1db093137967bb7a8faf1356be7f6a8da44ba
sam.sjk <sam.sjk@d8bbe975-9c3a-0410-8092-a9b24094b31a>
2009-03-04 16:28:15 +0000

    finishing authehticate/orize methods in Model_Helper_Auth

--- trunk/aabot/Model/Helper/Auth.php
+++ trunk/aabot/Model/Helper/Auth.php
@@ -17,6 +17,26 @@ class Model_Helper_Auth {
     const HASH_SALT = 'fhu&8(3bf76$@jjd-a *66D sjasdff[}J';
 
     const AUTHENTICATE_SESSION_KEY = '__auth';
+    /*
+     * ex: array(
+     *  0 => 'action_1',
+     *  1 => 'action_2'
+     *  )
+     */
+    private $actions_to_authenticate = array();
+    /*
+     * ex: array(
+     *  0 => array(
+     *      'actions' => array('add','edit'),
+     *      'groups' => array('steward', 'admin')
+     *  ),
+     *  1 => array(
+     *      'actions' => array('delete'),
+     *      'groups' => array('admin')
+     *  )
+     *  first match found is acted on (parsing starts at zero)
+     */
+    private $actions_to_authorize = array();
 
     private $db_handle = null;
 
@@ -36,7 +56,7 @@ class Model_Helper_Auth {
      * @param string $password
      * @return boolean
      */
-    public function authenticate($credential, $password=null) {
+    public function login($credential, $password=null) {
         if(is_array($credential)) {
             $credential = array_get_else($credential, 'auth',$credential);
             $password = array_get_else($credential, self::USER_PASSWORD_FEILD);
@@ -62,7 +82,66 @@ class Model_Helper_Auth {
 		}
         return $authenticated;
     }
+    /**
+     * Meant to be called in a Controller's init() method to set up what will
+     * authenticated.
+     *
+     * @param mixed [optiona] $actions_to_authenticate If given can be array or
+     * comma delim string.  If null, considered all actions
+     */
+    public function authenticate($actions_to_authenticate=null) {
+        if($actions_to_authenticate===null) {
+           $this->actions_to_authenticate = array('__ALL');
+        } else {
+            $this->actions_to_authenticate = !is_array($actions_to_authenticate)
+                ? explode(',', $actions_to_authenticate)
+                :$actions_to_authenticate;
+        }
+    }
+    /**
+     * Meant to be called in a Controller's init() method to set up what will
+     * authorized.
+     *
+     * @param mixed $authorized_groups array or commma delim string
+     * @param mixed [optional] $actions_to_authorize array or commma
+     * delim string.    If null, considered all actions
+     */
+    public function authorize($authorized_groups, $actions_to_authorize=null) {
+        $authorized_groups = !is_array($authorized_groups)
+                ? explode(',', $authorized_groups)
+                :$authorized_groups;
+        if($actions_to_authorize===null) {
+           $actions_to_authorize = array('__ALL');
+        } else {
+            $actions_to_authorize = !is_array($actions_to_authorize)
+                ? explode(',', $actions_to_authorize)
+                :$actions_to_authorize;
+        }
+        $this->actions_to_authorize[] = array('actions'=>$actions_to_authorize, 'groups' => $authorized_groups);
+    }
 
+    public function validate_credentials($requested_action) {
+        $valid_credentials = false;
+        // if no authentication set, return true
+        if(empty ($this->actions_to_authenticate) || ! in_array($requested_action, $this->actions_to_authenticate)) {
+            ENV::$log->debug(__METHOD__. "Requested action [{$requested_action}] passed authentication.  Not found in actions_to_authenticate ["
+                .implode(', ',$this->actions_to_authenticate)."]");
+           $valid_credentials = true;
+        } else if(in_array('__ALL', $this->actions_to_authenticate) || in_array($requested_action, $this->actions_to_authenticate)) {
+            if($this->loggedin()) {
+                // check for authorization
+                if(empty ($this->actions_to_authorize)) {
+                    $valid_credentials = true;
+                } else {
+                    $valid_credentials = $this->has_group_for_action($requested_action);
+                }
+            }
+        }
+        return $valid_credentials;
+    }
+    private function has_group_for_action($requested_action) {
+
+    }
     private function init_db() {
         if ($this->db_handle==null) {
             if($config = ENV::load_config_file('db_conf')) {
